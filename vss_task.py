3cc485ac50b10751522c54ae8adcde060d203ce9
Felipe Martins <fbm2@cin.ufpe.br>
2022-11-07 15:10:22 -0300

    experiment: slurm test

--- vss_task.py
+++ vss_task.py
@@ -26,7 +26,7 @@ def get_cfg():
         'rl_device': 'cuda:0',
         'sim_device': 'cuda:0',
         'graphics_device_id': 0,
-        'headless': False,
+        'headless': True,
         'virtual_screen_capture': False,
         'force_render': False,
         'physics_engine': 'physx',
