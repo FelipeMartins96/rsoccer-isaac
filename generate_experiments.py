11acff2e80441cd993a91751a6c481403ffac778
Felipe Martins <fbm2@cin.ufpe.br>
2023-02-10 01:11:57 +0000

    wip: update generate experiment to use exp-name_algo_seed folder name

--- generate_experiments.py
+++ generate_experiments.py
@@ -4,59 +4,57 @@ from itertools import combinations_with_replacement
 
 TOTAL_EPS = 1000
 ## Get folders list
-runs = os.listdir('runs')
 
-team = namedtuple('team', ['name', 'seed', 'algo', 'checkpoint'])
+team = namedtuple('team', ['experiment', 'seed', 'algo', 'checkpoint'])
+
+EXPERIMENT_A = 'teste'
+EXPERIMENT_B = 'teste-2'
 
 ## Get nn path, last and best
-teams = []
-for run in runs:
+teams_a = []
+teams_b = []
+
+runs = os.listdir('runs')
+for run in [r for r in runs if EXPERIMENT_A in r]:
     run_path = os.path.join('runs', run, 'nn')
 
+    # experiment-name_algo_seed
     ## get best nn
-    algo = run.split('_')[0]
-    name = '_'.join(run.split('_')[:-1])
+    experiment = run.split('_')[0]
+    algo = run.split('_')[1]
     seed = run.split('_')[-1]
     path = os.path.abspath(os.path.join(run_path, f'{run}.pth'))
-    teams.append(team(name, seed, algo, path))
+    teams_a.append(team(experiment, seed, algo, path))
 
-    # ## get last nn
-    # for ckpt in [p for p in os.listdir(run_path) if f'ep_{TOTAL_EPS}_rew' in p]:
-    #     path = os.path.abspath(os.path.join(run_path, ckpt))
-    #     teams.append(team(name, seed, 'last', path))
+    if algo == 'ppo':
+        teams_b.append(team(experiment, seed, algo + '-x3', path))
 
+for run in [r for r in runs if EXPERIMENT_B in r]:
+    run_path = os.path.join('runs', run, 'nn')
 
-path = os.path.abspath('ppo-0.pth')
-teams.append(team('ppo_oldEnv', '0', 'ppo', path))
-path = os.path.abspath('ppo-CMA-1.pth')
-teams.append(team('ppo-cma_oldEnv', '1', 'ppo-cma', path))
-path = os.path.abspath('ppo-DMA.pth')
-teams.append(team('ppo-dma_oldEnv', '', 'ppo-dma', path))
+    # experiment-name_algo_seed
+    ## get best nn
+    experiment = run.split('_')[0]
+    algo = run.split('_')[1]
+    seed = run.split('_')[-1]
+    path = os.path.abspath(os.path.join(run_path, f'{run}.pth'))
+    teams_b.append(team(experiment, seed, algo, path))
 
-## Add ppo-x3 cases
-vss3_teams = []
-for t in teams:
-    if t.algo == 'ppo':
-        vss3_teams.append(team(t.name + '_x3', t.seed, t.algo + '-x3', t.checkpoint))
-teams += vss3_teams
+    if algo == 'ppo':
+        teams_b.append(team(experiment, seed, algo + '-x3', path))
+
 
 ## Add OU and Zero cases
-teams += [team('ou', '', 'ou', ''), team('zero', '', 'zero', '')]
-
-## Get combinations
-teams_combinations = combinations_with_replacement(teams, 2)
+teams_a += [team(EXPERIMENT_A, '', 'ou', ''), team(EXPERIMENT_A, '', 'zero', '')]
+teams_b += [team(EXPERIMENT_B, '', 'ou', ''), team(EXPERIMENT_B, '', 'zero', '')]
 
 f = open('test_nets.sh', 'w')
+
 ## For each pair
 i = 0
-for team_comb in teams_combinations:
-    team_a, team_b = team_comb
-
-    if 'oldEnv' in team_a.name and 'oldEnv' in team_b.name:
-        continue
-    if not 'oldEnv' in team_a.name and not 'oldEnv' in team_b.name:
-        continue
-    f.write(
-        f'python test_net.py index={i} blue_team={team_a.name} blue_ckpt={team_a.checkpoint}  blue_seed={team_a.seed} blue_algo={team_a.algo} yellow_team={team_b.name} yellow_ckpt={team_b.checkpoint} yellow_seed={team_b.seed} yellow_algo={team_b.algo}\n'
-    )
-    i += 1
+for t_a in teams_a:
+    for t_b in teams_b:
+        f.write(
+            f'python test_net.py index={i} blue_exp={t_a.experiment} blue_algo={t_a.algo} blue_seed={t_a.seed} blue_ckpt={t_a.checkpoint} yellow_exp={t_b.experiment} yellow_algo={t_b.algo} yellow_seed={t_b.seed} yellow_ckpt={t_b.checkpoint}\n'
+        )
+        i+=1
